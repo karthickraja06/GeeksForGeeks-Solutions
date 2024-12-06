e8f23336c238aa86329f3ee080f0c542b3082ff7
Karthick Raja <126078435+karthickraja06@users.noreply.github.com>
2024-12-06 12:26:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int helper(Node head){
+        if(head == null){
+            return 1;
+        }
+        int carry = helper(head.next);
+        if(head.data + carry > 9){
+                head.data = 0;
+                return 1;
+        }else{
+            head.data += carry; 
+        }
+        return 0;
+    }
+    public Node addOne(Node head) {
+        int pad = helper(head);
+        Node one = new Node(1);
+        if(pad == 0)return head;
+        else{
+            one.next = head;
+        }
+        return one;
+    }
+}
